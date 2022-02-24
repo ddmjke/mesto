60a6299d64a1942a1a8505543b81c881fdb46b99
Dmitry Konychev <dalexeych@gmail.com>
2022-02-24 13:18:32 +0300

    fix: review fixes

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,14 @@ let profileInfo = profile.querySelector('.profile__description');
 let profileEdditButton = profile.querySelector('.profile__eddit-button');
 
 let popup = document.querySelector('.pop-up');
-let popupForm = popup.querySelector('.pop-up__container');
 let popupCloseButton = popup.querySelector('.pop-up__close-button');
-let popupInputs = popup.querySelectorAll('.pop-up__input')
+let popupForm = popup.querySelector('.pop-up__form');
+let popupUserName = popupForm.querySelector('.pop-up__input_field_name')
+let popupUserInfo = popupForm.querySelector('.pop-up__input_field_info')
 
 function openEddit() {
-  popupInputs[0].value = profileName.textContent;
-  popupInputs[1].value = profileInfo.textContent;
+  popupUserName.value = profileName.textContent;
+  popupUserInfo.value = profileInfo.textContent;
   popup.classList.add('pop-up_active');
 }
 function closeEddit(evt) {
@@ -19,9 +20,9 @@ function closeEddit(evt) {
 }
 function submitEddit(evt) {
   evt.preventDefault(); 
-  profileName.textContent = popupInputs[0].value;
-  profileInfo.textContent = popupInputs[1].value;
-  popup.classList.remove('pop-up_active');
+  profileName.textContent = popupUserName.value;
+  profileInfo.textContent = popupUserInfo.value;
+  closeEddit(evt);
 }
 
 profileEdditButton.addEventListener('click',openEddit);
