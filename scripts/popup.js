b277c0471cbe3e1bf8cd078e1355e076ea6c9f4d
Dmitry Konychev <dalexeych@gmail.com>
2022-04-24 20:03:25 +0300

    feat: popupwithimage class added

--- scripts/popup.js
+++ scripts/popup.js
@@ -25,3 +25,18 @@ class Popup {
     button.addEventListener('click', this._close);
   }
 }
+
+class PopupWithImage extends Popup {
+  constructor(selectorString) {
+    super(selectorString);
+    this._image = this._element.querySelector('.pop-up__image');
+    this._name = this._element.querySelector('.pop-up__image-caption');
+  }
+
+  open(link, name) {
+    this._image.src = link;
+    this._image.alt = name;
+    this._name.textContent = name;
+    Popup.open();
+  }
+}
