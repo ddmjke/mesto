af5bf4d4ab5dae8ae90d9e95715e5ade0c733c2c
Dmitry Konychev <dalexeych@gmail.com>
2022-04-27 22:04:50 +0300

    ref: small validator refactor

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -2,53 +2,56 @@ export default class FormValidator {
   constructor(keys, formElement) {
     this._keys = keys;
     this._form = formElement;
-    this._inputs = Array.from(this._form.querySelectorAll(this._keys.inputSelector));
     this._button = this._form.querySelector(this._keys.submitButtonSelector);
+    this._inputs = [];
+    const inputarray = Array.from(this._form.querySelectorAll(this._keys.inputSelector));
+    inputarray.forEach(input => {
+      const errorElement = this._form.querySelector(`.${input.id}-error`);
+      this._inputs.push({input: input, errorElement: errorElement});
+    });
   }
   
   enable() {
-    this._toggleButton(this._button, this._inputs);
+    this._toggleButton();
     this._setListeners();
   }
   
   _setListeners() {
     this._form.addEventListener('submit', (evt) => {
       evt.preventDefault();
-      this._toggleButton(this._button, this._inputs, true);
+      this._toggleButton(true);
     });
-    this._inputs.forEach((input) => {
-      input.addEventListener('input', (evt) => {
-        this._validateInput(input);
-        this._toggleButton(this._button, this._inputs);
+    this._inputs.forEach((pair) => {
+      pair.input.addEventListener('input', (evt) => {
+        this._validateInput(pair);
+        this._toggleButton();
       })
     });
   }
 
-  _toggleButton(button, inputs, forceDisable) {
-    const condition = forceDisable || this._hasInvalidInputs(inputs);
-    if (condition) {
-      button.setAttribute('disabled', '');
-      button.classList.add(this._keys.inactiveButtonClass);
+  _toggleButton(forceDisable) {
+    if (this._hasInvalidInputs() || forceDisable) {
+      this._button.setAttribute('disabled', '');
+      this._button.classList.add(this._keys.inactiveButtonClass);
     } else {
-      button.removeAttribute('disabled');
-      button.classList.remove(this._keys.inactiveButtonClass);
+      this._button.removeAttribute('disabled');
+      this._button.classList.remove(this._keys.inactiveButtonClass);
     }
   }
 
-  _validateInput(input, forceValid) {
-    const errorElement = this._form.querySelector(`.${input.id}-error`);
-    if (input.validity.valid || forceValid) {
-      errorElement.classList.remove(this._keys.errorClass);
-      input.classList.remove(this._keys.inputErrorClass);
+  _validateInput(pair, forceValid) {
+    if (pair.input.validity.valid || forceValid) {
+      pair.errorElement.classList.remove(this._keys.errorClass);
+      pair.input.classList.remove(this._keys.inputErrorClass);
     } else {
-      errorElement.classList.add(this._keys.errorClass);
-      errorElement.textContent = input.validationMessage;
-      input.classList.add(this._keys.inputErrorClass);
+      pair.errorElement.classList.add(this._keys.errorClass);
+      pair.errorElement.textContent = pair.input.validationMessage;
+      pair.input.classList.add(this._keys.inputErrorClass);
     }
   }
 
-  _hasInvalidInputs(inputs) {
-    return inputs.some((input) => {return !input.validity.valid});
+  _hasInvalidInputs() {
+    return this._inputs.some((pair) => {return !pair.input.validity.valid});
   }
 
   //============ public method to hide input errors when form just opened
